a92d7be9f919927eaa23ef5a998cf61650d4ab0d
chanseokpoh <chanseokpoh@gmail.com>
2024-12-26 15:07:53 -0800

    moved game logic from players to env

--- envs/lands_env.py
+++ envs/lands_env.py
@@ -3,11 +3,15 @@ import gymnasium as gym
 from gymnasium.spaces import Box, Dict, Discrete, MultiDiscrete
 import json
 import traceback
+import lands_vars as lv
 
 class LandsEnv(gym.Env):
     # TODO: action space and other initializations
     def __init__(
         self,
+        num_elements=5,
+        num_cards=5,
+        start_hand=5
         # Params
     ):
         # 5 colors, 5 cards, own hand + 2 fields
@@ -17,11 +21,58 @@ class LandsEnv(gym.Env):
         # Prob need tuple or something for card effects
         self.action_space = Discrete(6)
 
-        # self.board = init_board()
+        # 0=p1, 1=p2
+        self.turn = 0
 
         # self.render_mode = None
         # self.num_steps = 0
         # self.max_steps = max_steps
+
+        # Game meta variables initialization
+        self.num_elements = num_elements
+        self.num_cards = num_cards
+        self.start_hand = start_hand
+
+        # Game variables initialization
+        self.field = np.zeros((2, lv.NUM_ELEMENTS))       # p1 = first row, p2 = second row
+        self.p1 = {"field": np.zeros((2, lv.NUM_ELEMENTS)), "hand": np.zeros((lv.NUM_ELEMENTS)), "discard": np.zeros((lv.NUM_ELEMENTS)), "deck": None}
+        self.p2 = {"field": np.zeros((2, lv.NUM_ELEMENTS)), "hand": np.zeros((lv.NUM_ELEMENTS)), "discard": np.zeros((lv.NUM_ELEMENTS)), "deck": None}
+        self._init_deck(self.p1)
+        self._init_deck(self.p2)
+        self.draw_n(self.p1, self.start_hand)
+        self.draw_n(self.p2, self.start_hand)
+
+    # Shuffle the deck
+    def _shuffle_deck(self, player):
+        np.random.shuffle(player["deck"])
+
+    # Initialize deck
+    def _init_deck(self, player):
+        player["deck"] = np.array([(i)//(self.num_cards) + 1 for i in range(self.num_elements*self.num_cards)])
+        self._shuffle_deck(player)
+
+    # Add all cards in discard pile to the deck and shuffle
+    # return (new_deck, new_discard)
+    def putDiscardToDeck(self, player):
+        player["deck"].append(np.array([[n+1]*i for n, i in enumerate(player["discard"])]).reshape(-1))
+        player["discard"].fill(0)
+        self._shuffle_deck(player)
+
+    # Draw one card from the deck to the hand
+    def draw_n(self, player, n=1):
+        if len(player["deck"]) < 1:
+            self.putDiscardToDeck(player)
+            assert len(player["deck"]) > 0    # We want deck to contain at least 1 card
+        for i in range(n):
+            player["hand"][player["deck"][i]-1] += 1
+        player["deck"] = player["deck"][n:]
+
+    # reset state of a player
+    def resetStates(self, player):
+        player["hand"].fill(0)
+        player["discard"].fill(0)
+        self._init_deck(player)
+        self.draw_n(player, self.start_hand)
 
     # TODO: unimplemented
     def reset(self, seed=None, options=None):
