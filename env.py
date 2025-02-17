6e2f8444c64edf50df45405e08f0c4743eedd5b5
Kasten Welsh <welshkas000@gmail.com>
2025-02-17 14:46:29 -0800

    comment

--- env.py
+++ env.py
@@ -86,7 +86,7 @@ def get_image(path):
 
 def env(**kwargs):
     env = raw_env(**kwargs)
-    # env = wrappers.TerminateIllegalWrapper(env, illegal_reward=-1)
+    env = wrappers.TerminateIllegalWrapper(env, illegal_reward=-1)
     env = wrappers.AssertOutOfBoundsWrapper(env)
     env = wrappers.OrderEnforcingWrapper(env)
     return env
